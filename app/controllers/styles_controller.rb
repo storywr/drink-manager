ef41d80db090fe4bdec408d343450e68ede018e8
Bill <storywr@gmail.com>
2017-02-04 02:21:01 -0500

    added style form

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -8,4 +8,20 @@ class StylesController < ApplicationController
     @style = Style.find(params[:id])
   end
 
+  def new
+    @style = Style.new
+  end
+
+  def create
+    @style = Style.create(style_params)
+    redirect_to style_path(@style)
+  end
+
+  private
+
+  def style_params
+    params.require(:style).permit(:name, :description)
+  end
+
+
 end
