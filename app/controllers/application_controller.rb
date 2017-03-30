2b70aabce5212280136a4c18d72e43ece189c3ff
Bill <storywr@gmail.com>
2017-03-30 01:37:18 -0400

    fixed authenticity error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
 
   def current_user
     @user = (User.find_by(id: session[:user_id]) || User.new)
