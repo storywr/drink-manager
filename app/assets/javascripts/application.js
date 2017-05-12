152ac46ac4459f15de76c9b045e5c31ccfc72037
Bill <storywr@gmail.com>
2017-05-12 00:01:40 -0400

    order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
 //= require jquery.turbolinks
-//= require bootstrap
 //= require turbolinks
